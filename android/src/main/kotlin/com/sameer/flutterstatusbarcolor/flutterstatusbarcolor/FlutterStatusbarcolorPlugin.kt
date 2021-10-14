b1623ce7de0eb5b07df9dbe3e945b8a3f539b6bc
Sameer Kashyap <40424087+Sameerkash@users.noreply.github.com>
2021-10-14 17:23:37 +0530

    Revert "Fix all Kotlin errors + sample v2 upgrade"

--- android/src/main/kotlin/com/sameer/flutterstatusbarcolor/flutterstatusbarcolor/FlutterStatusbarcolorPlugin.kt
+++ android/src/main/kotlin/com/sameer/flutterstatusbarcolor/flutterstatusbarcolor/FlutterStatusbarcolorPlugin.kt
@@ -1,36 +1,32 @@
 package com.sameer.flutterstatusbarcolor.flutterstatusbarcolor
 
-import android.animation.ValueAnimator
-import android.app.Activity
 import android.os.Build
+import android.app.Activity
 import android.view.View
-import androidx.annotation.NonNull
-import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.embedding.engine.plugins.activity.ActivityAware
-import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
-import io.flutter.plugin.common.MethodCall
+import android.animation.ValueAnimator
 import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.PluginRegistry.Registrar
 
-
-class FlutterStatusbarcolorPlugin : FlutterPlugin, MethodChannel.MethodCallHandler, ActivityAware {
-
-    private var mActivity: Activity? = null
-    private var channel: MethodChannel? = null
-
-
-    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "plugins.sameer.com/statusbar")
-        channel?.setMethodCallHandler(this)
+class FlutterStatusbarcolorPlugin private constructor(private val activity: Activity?) : MethodCallHandler {
+    companion object {
+        @JvmStatic
+        fun registerWith(registrar: Registrar): Unit {
+            val channel = MethodChannel(registrar.messenger(), "plugins.sameer.com/statusbar")
+            channel.setMethodCallHandler(FlutterStatusbarcolorPlugin(registrar.activity()))
+        }
     }
 
-    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: MethodChannel.Result) {
-        if (mActivity == null) return result.success(null)
+    override fun onMethodCall(call: MethodCall, result: Result): Unit {
+        if (activity == null) return result.success(null)
 
         when (call.method) {
             "getstatusbarcolor" -> {
-                var statusBarColor = 0
+                var statusBarColor: Int = 0
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    statusBarColor = mActivity!!.window.statusBarColor
+                    statusBarColor = activity.window.statusBarColor
                 }
                 result.success(statusBarColor)
             }
@@ -39,36 +35,31 @@ class FlutterStatusbarcolorPlugin : FlutterPlugin, MethodChannel.MethodCallHandl
                 val animate: Boolean = call.argument("animate")!!
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                     if (animate) {
-                        val colorAnim =
-                            ValueAnimator.ofArgb(mActivity!!.window.statusBarColor, statusBarColor)
-                        colorAnim.addUpdateListener { anim ->
-                            mActivity!!.window.statusBarColor = anim.animatedValue as Int
-                        }
-                        colorAnim.duration = 300
+                        val colorAnim = ValueAnimator.ofArgb(activity.window.statusBarColor, statusBarColor)
+                        colorAnim.addUpdateListener { anim -> activity.window.statusBarColor = anim.animatedValue as Int }
+                        colorAnim.setDuration(300)
                         colorAnim.start()
                     } else {
-                        mActivity!!.window.statusBarColor = statusBarColor
+                        activity.window.statusBarColor = statusBarColor
                     }
                 }
                 result.success(null)
             }
             "setstatusbarwhiteforeground" -> {
-                val useWhiteForeground: Boolean = call.argument("whiteForeground")!!
+                val usewhiteforeground: Boolean = call.argument("whiteForeground")!!
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    if (useWhiteForeground) {
-                        mActivity!!.window.decorView.systemUiVisibility =
-                            mActivity!!.window.decorView.systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
+                    if (usewhiteforeground) {
+                        activity.window.decorView.systemUiVisibility = activity.window.decorView.systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
                     } else {
-                        mActivity!!.window.decorView.systemUiVisibility =
-                            mActivity!!.window.decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                        activity.window.decorView.systemUiVisibility = activity.window.decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
                     }
                 }
                 result.success(null)
             }
             "getnavigationbarcolor" -> {
-                var navigationBarColor = 0
+                var navigationBarColor: Int = 0
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    navigationBarColor = mActivity!!.window.navigationBarColor
+                    navigationBarColor = activity.window.navigationBarColor
                 }
                 result.success(navigationBarColor)
             }
@@ -77,56 +68,28 @@ class FlutterStatusbarcolorPlugin : FlutterPlugin, MethodChannel.MethodCallHandl
                 val animate: Boolean = call.argument("animate")!!
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                     if (animate) {
-                        val colorAnim = ValueAnimator.ofArgb(
-                            mActivity!!.window.navigationBarColor,
-                            navigationBarColor
-                        )
-                        colorAnim.addUpdateListener { anim ->
-                            mActivity!!.window.navigationBarColor = anim.animatedValue as Int
-                        }
-                        colorAnim.duration = 300
+                        val colorAnim = ValueAnimator.ofArgb(activity.window.navigationBarColor, navigationBarColor)
+                        colorAnim.addUpdateListener { anim -> activity.window.navigationBarColor = anim.animatedValue as Int }
+                        colorAnim.setDuration(300)
                         colorAnim.start()
                     } else {
-                        mActivity!!.window.navigationBarColor = navigationBarColor
+                        activity.window.navigationBarColor = navigationBarColor
                     }
                 }
                 result.success(null)
             }
             "setnavigationbarwhiteforeground" -> {
-                val useWhiteForeground: Boolean = call.argument("whiteForeground")!!
+                val usewhiteforeground: Boolean = call.argument("whiteForeground")!!
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    if (useWhiteForeground) {
-                        mActivity!!.window.decorView.systemUiVisibility =
-                            mActivity!!.window.decorView.systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR.inv()
+                    if (usewhiteforeground) {
+                        activity.window.decorView.systemUiVisibility = activity.window.decorView.systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR.inv()
                     } else {
-                        mActivity!!.window.decorView.systemUiVisibility =
-                            mActivity!!.window.decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+                        activity.window.decorView.systemUiVisibility = activity.window.decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
                     }
                 }
                 result.success(null)
             }
             else -> result.notImplemented()
         }
-    }
-
-    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-        channel?.setMethodCallHandler(null)
-        channel = null
-    }
-
-    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-        mActivity = binding.activity
-    }
-
-    override fun onDetachedFromActivityForConfigChanges() {
-        channel?.setMethodCallHandler(null)
-    }
-
-    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-        channel?.setMethodCallHandler(FlutterStatusbarcolorPlugin())
-    }
-
-    override fun onDetachedFromActivity() {
-        channel?.setMethodCallHandler(null)
     }
 }
